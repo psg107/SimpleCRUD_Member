dc04c71efaa2aa5d3699f06a6a4de222b75d9c5f
psg107 <psg107@naver.com>
2022-02-14 18:54:45 +0900

    first commit

---
+++ src/main/java/com/example/simplecrud_member/SimpleCrudMemberApplication.java
@@ -0,0 +1,13 @@
+package com.example.simplecrud_member;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleCrudMemberApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleCrudMemberApplication.class, args);
+    }
+
+}
